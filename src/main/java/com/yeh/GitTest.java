a2cd4e77cf9cfd927819d847b0c4ca7bf254cf9f
yeh <yeh@19105.com>
2021-04-26 17:57:56 +0800

    first commit

---
+++ src/main/java/com/yeh/GitTest.java
@@ -0,0 +1,13 @@
+package com.yeh;
+
+/**
+ * @Auther: zhaoss
+ * @Date: 2021/4/26 - 04 - 26 - 17:54
+ * @Description: com.yeh
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
